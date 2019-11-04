fe7564dcdac8313b9e2cfd16e4ff414e87cd8727
lukewoodcock <lukewoodcock@gmail.com>
2019-11-04 10:14:31 +0100

    WIP add a couple more tests. TODO - need to tidy/refactor, big time

--- test/BasicDistributionTest.scala
+++ test/BasicDistributionTest.scala
@@ -1,4 +1,5 @@
 import java.util.Calendar
+import scala.util.Random
 
 import controllers.HomeController
 import model.{Family, Shift, ShiftType}
@@ -7,6 +8,8 @@ import services.ShiftManager
 
 class BasicDistributionTest extends FunSuite {
 
+  //TODO - add restrictions / preferences
+  //TODO - prioritise families for week if previous week they don't meet weekly quota
 
   test("remove date") {
     def mockDay(n:Int):List[Shift] = List[Shift](
@@ -16,7 +19,7 @@ class BasicDistributionTest extends FunSuite {
       Shift("c".concat(n.toString), ShiftType(Shift.NAMES.CLOSING, Shift.TYPES.ORGANISE, 1.5), n, None)
     )
 
-    def mockWeek():List[Shift] = mockDay(Calendar.MONDAY) ::: mockDay(Calendar.TUESDAY) ::: mockDay(Calendar.WEDNESDAY) ::: mockDay(Calendar.THURSDAY) ::: mockDay(Calendar.FRIDAY)
+    def mockWeek(n:Int):List[Shift] = mockDay(Calendar.MONDAY + n) ::: mockDay(Calendar.TUESDAY + n) ::: mockDay(Calendar.WEDNESDAY + n) ::: mockDay(Calendar.THURSDAY + n) ::: mockDay(Calendar.FRIDAY + n)
 
     def mockFamilies(): List[Family] = {
       val l = Family("Lautaro")
@@ -30,7 +33,7 @@ class BasicDistributionTest extends FunSuite {
     }
 
     val families = mockFamilies()
-    val shifts = mockWeek()
+    val shifts = mockWeek(0)
 
     val contenders = ShiftManager.removeByDate(families, Calendar.MONDAY)
     assert(contenders.exists(f => f.id == "Lautaro") == false)
@@ -131,7 +134,7 @@ class BasicDistributionTest extends FunSuite {
     assert(titi.exists(f => f.id == "Emma") == false)
   }
 
-  test("Basic autoFill") {
+  test("Basic autoFill week") {
     def mockDay(n:Int):List[Shift] = List[Shift](
       Shift("o".concat(n.toString), ShiftType(Shift.NAMES.OPENING, Shift.TYPES.ORGANISE, 1.5), n, None),
       Shift("m".concat(n.toString), ShiftType(Shift.NAMES.MORNING_MOYENS, Shift.TYPES.GUARD, 4.0), n, None),
@@ -139,7 +142,7 @@ class BasicDistributionTest extends FunSuite {
       Shift("c".concat(n.toString), ShiftType(Shift.NAMES.CLOSING, Shift.TYPES.ORGANISE, 1.5), n, None)
     )
 
-    def mockWeek():List[Shift] = mockDay(Calendar.MONDAY) ::: mockDay(Calendar.TUESDAY) ::: mockDay(Calendar.WEDNESDAY) ::: mockDay(Calendar.THURSDAY) ::: mockDay(Calendar.FRIDAY)
+    def mockWeek(n:Int):List[Shift] = mockDay(Calendar.MONDAY + n) ::: mockDay(Calendar.TUESDAY + n) ::: mockDay(Calendar.WEDNESDAY + n) ::: mockDay(Calendar.THURSDAY + n) ::: mockDay(Calendar.FRIDAY + n)
 
     def mockFamilies(): List[Family] = {
       val names = List("Lautaro", "Emma", "Elisa", "Gabriel", "Florentin")
@@ -147,7 +150,7 @@ class BasicDistributionTest extends FunSuite {
     }
 
     val families = mockFamilies()
-    val shifts = mockWeek()
+    val shifts = mockWeek(0) //::: mockWeek(7) ::: mockWeek(14)
 
     val out: List[(Shift, Option[Family])] = ShiftManager.autoFill(shifts, families)
 //    for(s <- out) {
@@ -162,7 +165,38 @@ class BasicDistributionTest extends FunSuite {
 //    assert(!families.exists(f => f.shifts.size > 3))
   }
 
-    test("2 family guard autoFill") {
+  test("Basic autoFill 2 weeks") {
+    def mockDay(n:Int):List[Shift] = List[Shift](
+      Shift("o".concat(n.toString), ShiftType(Shift.NAMES.OPENING, Shift.TYPES.ORGANISE, 1.5), n, None),
+      Shift("m".concat(n.toString), ShiftType(Shift.NAMES.MORNING_MOYENS, Shift.TYPES.GUARD, 4.0), n, None),
+      Shift("a".concat(n.toString), ShiftType(Shift.NAMES.AFTERNOON_MOYENS, Shift.TYPES.GUARD, 4.0), n, None),
+      Shift("c".concat(n.toString), ShiftType(Shift.NAMES.CLOSING, Shift.TYPES.ORGANISE, 1.5), n, None)
+    )
+
+    def mockWeek(n:Int):List[Shift] = mockDay(Calendar.MONDAY + n) ::: mockDay(Calendar.TUESDAY + n) ::: mockDay(Calendar.WEDNESDAY + n) ::: mockDay(Calendar.THURSDAY + n) ::: mockDay(Calendar.FRIDAY + n)
+
+    def mockFamilies(): List[Family] = {
+      val names = List("Lautaro", "Emma", "Elisa", "Gabriel", "Florentin")
+      names.map(f => Family(f))
+    }
+
+    val families = mockFamilies()
+    val shifts = mockWeek(0) //::: mockWeek(7) ::: mockWeek(14)
+
+    val out: List[(Shift, Option[Family])] = ShiftManager.autoFill(mockWeek(0), Random.shuffle(mockFamilies())) ::: ShiftManager.autoFill(mockWeek(7), Random.shuffle(mockFamilies()))
+        for(s <- out) {
+          println("Shift:" + s)
+        }
+
+    for(fam <- families.filter(f => f.shifts.toList.size > 3)) {
+      println("Family:" + fam)
+    }
+
+//    assert(out.size == 20)
+        assert(!families.exists(f => f.shifts.size > 3))
+  }
+
+  test("2 family guard autoFill") {
     def mockDay(date:Int):List[Shift] = List[Shift](
       Shift(Shift.NAMES.OPENING.concat(date.toString), ShiftType(Shift.NAMES.OPENING, Shift.TYPES.ORGANISE, 1.5), date, None),
       Shift(Shift.NAMES.MORNING_MOYENS.concat(date.toString), ShiftType(Shift.NAMES.MORNING_MOYENS, Shift.TYPES.GUARD, 4.0), date, None),
@@ -172,9 +206,7 @@ class BasicDistributionTest extends FunSuite {
       Shift(Shift.NAMES.CLOSING.concat(date.toString), ShiftType(Shift.NAMES.CLOSING, Shift.TYPES.ORGANISE, 1.5), date, None)
     )
 
-    def mockWeek():List[Shift] = mockDay(Calendar.MONDAY) ::: mockDay(Calendar.TUESDAY) ::: mockDay(Calendar.WEDNESDAY) ::: mockDay(Calendar.THURSDAY) ::: mockDay(Calendar.FRIDAY)
-//      ::: mockDay(Calendar.MONDAY + 7) ::: mockDay(Calendar.TUESDAY + 7) ::: mockDay(Calendar.WEDNESDAY + 7) ::: mockDay(Calendar.THURSDAY + 7) ::: mockDay(Calendar.FRIDAY + 7)
-//      ::: mockDay(Calendar.MONDAY + 14) ::: mockDay(Calendar.TUESDAY + 14) ::: mockDay(Calendar.WEDNESDAY + 14) ::: mockDay(Calendar.THURSDAY + 14) ::: mockDay(Calendar.FRIDAY + 14)
+    def mockWeek(n:Int):List[Shift] = mockDay(Calendar.MONDAY + n) ::: mockDay(Calendar.TUESDAY + n) ::: mockDay(Calendar.WEDNESDAY + n) ::: mockDay(Calendar.THURSDAY + n) ::: mockDay(Calendar.FRIDAY + n)
 
     def mockFamilies(): List[Family] = {
 //      val names = List("Lautaro", "Emma", "Elisa", "Gabriel", "Florentin")
@@ -183,12 +215,53 @@ class BasicDistributionTest extends FunSuite {
     }
 
     val families = mockFamilies()
-    val shifts = mockWeek()
+    val shifts = mockWeek(0)
 
     val out: List[(Shift, Option[Family])] = ShiftManager.autoFill(shifts, families)
     for(s <- out) {
       println("Shift:" + s)
     }
+
+    for(f <- families) {
+      println("\n".concat(f.id))
+      for(s <- f.shifts) {
+        println(s.shiftType.shiftType.concat(" - ").concat(s.shiftType.id).concat(" - ").concat(s.date.toString))
+      }
+    }
+    assert(out.size == 30)
+    assert(!families.exists(f => f.shifts.size > 3))
+    assert(!families.exists(f => f.shifts.count(s => s.shiftType.shiftType == Shift.TYPES.ORGANISE) > 1))
+    assert(!families.exists(f => f.shifts.count(s => s.shiftType.shiftType == Shift.TYPES.GUARD) > 2))
+    //TODO write assert that no family has a >1 shift on the same day
+  }
+
+  test("2 family guard, 3 weeks autoFill") {
+    def mockDay(date:Int):List[Shift] = List[Shift](
+      Shift(Shift.NAMES.OPENING.concat(date.toString), ShiftType(Shift.NAMES.OPENING, Shift.TYPES.ORGANISE, 1.5), date, None),
+      Shift(Shift.NAMES.MORNING_MOYENS.concat(date.toString), ShiftType(Shift.NAMES.MORNING_MOYENS, Shift.TYPES.GUARD, 4.0), date, None),
+      Shift(Shift.NAMES.MORNING_GRANDS.concat(date.toString), ShiftType(Shift.NAMES.MORNING_GRANDS, Shift.TYPES.GUARD, 4.0), date, None),
+      Shift(Shift.NAMES.AFTERNOON_MOYENS.concat(date.toString), ShiftType(Shift.NAMES.AFTERNOON_MOYENS, Shift.TYPES.GUARD, 4.0), date, None),
+      Shift(Shift.NAMES.AFTERNOON_GRANDS.concat(date.toString), ShiftType(Shift.NAMES.AFTERNOON_GRANDS, Shift.TYPES.GUARD, 4.0), date, None),
+      Shift(Shift.NAMES.CLOSING.concat(date.toString), ShiftType(Shift.NAMES.CLOSING, Shift.TYPES.ORGANISE, 1.5), date, None)
+    )
+
+    def mockWeek(n:Int):List[Shift] = mockDay(Calendar.MONDAY + n) ::: mockDay(Calendar.TUESDAY + n) ::: mockDay(Calendar.WEDNESDAY + n) ::: mockDay(Calendar.THURSDAY + n) ::: mockDay(Calendar.FRIDAY + n)
+
+    def mockFamilies(): List[Family] = {
+      //      val names = List("Lautaro", "Emma", "Elisa", "Gabriel", "Florentin")
+      val names = List("Lautaro", "Emma", "Elisa", "Gabriel", "Florentin", "Aaron", "Francisca", "Timothy")
+      names.map(f => Family(f))
+    }
+
+    val families = mockFamilies()
+    val shifts = mockWeek(0)
+
+    val out: List[(Shift, Option[Family])] = ShiftManager.autoFill(mockWeek(0), mockFamilies()) ::: ShiftManager.autoFill(mockWeek(7), mockFamilies()) ::: ShiftManager.autoFill(mockWeek(14), mockFamilies())
+    for(s <- out) {
+      println("Shift:" + s)
+    }
+
+    //TODO - from out, get families by name and create list of their shifts
 
     for(f <- families) {
       println("\n".concat(f.id))
